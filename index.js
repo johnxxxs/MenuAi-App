b1e278391bda52c9f265757788e2b8e493754177
johnxxxs <systek.jgiraldo@gmail.com>
2026-06-06 16:05:33 +0200

    Fix import statement for pdf-parse module

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 const fs = require("fs");
-const pdf = require("pdf-parse").default;
+const pdf = require("pdf-parse");
 const express = require("express");
 const path = require("path");
 const multer = require("multer");
